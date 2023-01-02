b69c13f004c2df084941ceb782664e5cc6e06e4e
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2023-01-02 02:30:05 +0200

    upload basic packages and plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import disableDevtool from 'disable-devtool';
+import { TranslationService } from './services/translation.service';
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qpmo-admin-panel';
+  currentLanguage: any = '';
+  constructor(
+    public translate: TranslateService,
+    private _TranslationService: TranslationService
+  ) {
+    this.currentLanguage = localStorage.getItem('currentLanguage') || 'ar';
+    this.translate.use(this.currentLanguage);
+    this._TranslationService.currentLang(this.currentLanguage);
+    const body = document.getElementsByTagName('body');
+    this._TranslationService.currentlang.subscribe((lang) => {
+      // this if condition to check direction of all project according to current language
+      if (lang == 'ar') {
+        body[0].setAttribute('dir', 'rtl');
+      } else {
+        body[0].setAttribute('dir', 'ltr');
+      }
+    });
+
+    // to disable inspect element, f12 button and developer tools
+    disableDevtool();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MyIntercetor } from './intercetor';
+import {  HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +17,37 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'ar',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MyIntercetor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
+
+// this method make you able to use ngx-translate
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
